f9d4db4509f77cbee237ab35a7b56de9e4e6fba9
Guillaume Pavé <guillaumepave@gmail.com>
2021-04-12 21:39:16 +0200

    change timeline data

--- twitterdata.py
+++ twitterdata.py
@@ -1,8 +1,8 @@
 import twint
-import nest_asyncio
+#import nest_asyncio
 import datetime as dt
 
-nest_asyncio.apply()
+#nest_asyncio.apply()
 # Configure
 
 TWITTER_USERNAMES = {
@@ -35,11 +35,11 @@ c.Search = "Bitcoin"
 c.Custom["tweet"] = ["id", "created_at","username","tweet"]
 c.Verified = True
 c.Lang = "en"
-c.Min_likes = 500 # min like pour que le tweet soit sélectionner
+c.Min_likes = 200 # min like pour que le tweet soit sélectionner
 c.Min_replies = 200 # min replies
 c.Min_retweets = 100 # min retweets
 c.Output = f"bitcoin.json"
-c.Since = "2020-01-12"
+c.Since = "2012-01-01"
 c.Until = "2021-01-01"
 c.Store_json = True
 c.Hide_output = False
