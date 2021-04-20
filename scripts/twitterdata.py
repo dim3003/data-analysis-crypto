891b3453aedbdd64274e5131643fbeedbf46c5af
dim3003 <dimitri.andre@unil.ch>
2021-04-20 13:14:51 +0200

    get raw data for BTC ETH EOS

--- scripts/twitterdata.py
+++ scripts/twitterdata.py
@@ -3,7 +3,7 @@ import twint
 import datetime as dt
 
 #nest_asyncio.apply()
-# Configure
+
 
 TWITTER_USERNAMES = {
     'ts':'traderstewie',
@@ -13,37 +13,34 @@ TWITTER_USERNAMES = {
     'tt': 'tmltrader',
     'wsb': "wallstreetbets"
 }
-    
-# for key, value in TWITTER_USERNAMES.items():
-
-#     c = twint.Config()
-#     c.Username = value
-#     c.Lang = "en"
-#     #c.Geo = "48.880048,2.385939,5km"
-#     c.Limit = 3200
-#     c.Output = f"db/tweets/{key}/{key}_{dt.date.today()}.json"
-#     c.Store_json = True
-#     c.Hide_output = True
-    
-#     tweets = twint.run.Search(c)
 
 if __name__ == '__main__':
-    name = str(input("Which crypto: ")) #Ticker de la crypto
-    c = twint.Config()
 
+    #select keywords for each crypto we will train our model on
 
-    c.Search = name
-    #c.Limit = 10
-    c.Custom["tweet"] = ["id", "created_at","username","tweet"]
-    c.Verified = True
-    c.Lang = "en"
-    c.Min_likes = 0 # min like pour que le tweet soit sélectionner
-    c.Min_replies = 10 # min replies
-    c.Min_retweets = 0 # min retweets
-    c.Output = f"{name}.json"
-    c.Since = "2012-01-01"
-    c.Until = "2021-04-01"
-    c.Store_json = True
-    c.Hide_output = False
-        
-    tweets = twint.run.Search(c)
+    names = []
+
+    bitcoin = ["BTC", "Bitcoin", "bitcoin"]
+    names.append(bitcoin)
+    ethereum = ["ETH", "Ethereum", "ethereum"]
+    names.append(ethereum)
+    eos = ["EOS"]
+    names.append(eos)
+
+    #look for tweets for each crypto
+    for i in names:
+        name = i
+        c = twint.Config()
+
+        c.Search = name
+        c.Custom["tweet"] = ["id", "created_at","username","tweet"]
+        c.Verified = True
+        c.Lang = "en"
+        c.Min_replies = 10 # min replies
+        c.Output = f"../data/raw/{name[0]}.json"
+        c.Since = "2017-08-17"
+        c.Until = "2021-04-19"
+        c.Store_json = True
+        c.Hide_output = False
+
+        tweets = twint.run.Search(c)
